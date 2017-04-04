1b7a061fd31acac89edb5addaf205f4896014cc5
ariel.romero <arielromero@gmail.com>
2017-04-04 17:14:47 -0300

    first commit

---
+++ admPersonas/admPersonas/Entidades/Persona.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace admPersonas.Entidades
+{
+    class Persona
+    {
+        int id;
+        string nombre;
+        string apellido;
+        DateTime fechanacimiento;
+        int nro_documento;
+
+        public Persona(int id, string nombre, string apellido, DateTime fechanacimiento, int nro_documento)
+        {
+            this.id = id;
+            this.apellido = apellido;
+            this.nombre = nombre;
+            this.fechanacimiento = fechanacimiento;
+            this.nro_documento = nro_documento;
+        }
+    }
+}
